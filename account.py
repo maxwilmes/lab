bccd330dc8fda0ea6eb58f3099aad6438cb963de
maxwilmes <118315518+maxwilmes@users.noreply.github.com>
2022-11-15 17:13:33 -0600

    Add files via upload

---
+++ account.py
@@ -0,0 +1,30 @@
+
+
+class Account:
+    def __init__(self,name):
+        self.__account_balance = 0
+        self.__account_name = name
+
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+
+    def withdraw(self,amount):
+        if (amount > 0)  & (amount <= self.__account_balance):
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+
+    def get_balance(self):
+        return self.__account_balance
+
+
+    def get_name(self):
+        return self.__account_name
